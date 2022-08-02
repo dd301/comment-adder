f70368f70a32c308262b85ee96b034f91eaa7f74
dd301 <donatodoley1@gmail.com>
2022-08-02 15:43:19 +0530

    added enter key interactivity

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ var main = function() {
             $new_comment = $("<p>").text($(".comment-input input").val());
             
             $(".comments").append($new_comment);
+            $(".comment-input input").val("");
+        }
+    });
+
+    $(".comment-input input").on("keypress", function(event){
+        var $new_comment;
+        if (event.keyCode == 13){
+            if ($(".comment-input input").val() !== "")
+            {
+                $new_comment = $("<p>").text($(".comment-input input").val());
+                
+                $(".comments").append($new_comment);
+                $(".comment-input input").val("");
+            }
         }
     });
 };
